d9816de9f6b420f31f2f88cdd203a123a0154d4c
SmellyCat <smellycat2014@foxmail.com>
2015-09-10 21:27:23 +0800

    Fix bug run.py

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from connecting import app, views
 from connecting import debug_logging, online_logging
 
 if __name__ == '__main__':
-    online_logging('log\connecting.log')
-    app.run(port=8078, threaded=True)
+    online_logging(u'logs/connecting.log')
+    app.run(host='0.0.0.0', port=8078, threaded=True)
