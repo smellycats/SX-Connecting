7b053dea3871c5a7e868c199798485a03fdc0028
SmellyCat <smellycat2014@foxmail.com>
2015-09-18 21:58:58 +0800

    Update larger app style

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-from connecting import app, views
-from connecting import debug_logging, online_logging
+from connecting import app
 
 if __name__ == '__main__':
-    online_logging(u'logs/connecting.log')
-    app.run(host='0.0.0.0', port=8078, threaded=True)
+    app.run(host='0.0.0.0', threaded=True)
